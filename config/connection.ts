2b8ee8445a814e50a2a9f7b32f30ef6708a086ea
Cadence K <cadencehiggins@gmail.com>
2024-07-24 16:12:37 -0500

    created connection.ts

---
+++ config/connection.ts
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'postgres',
+        }
+    );
+}
+
+module.exports = sequelize;
