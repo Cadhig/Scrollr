69bd83944c6eea22d7bff42c7d14fbf0efbe0656
Cadence K <cadencehiggins@gmail.com>
2024-07-24 17:15:44 -0500

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Login from './routes/Login.tsx'
+import Signup from './routes/Signup.tsx';
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
